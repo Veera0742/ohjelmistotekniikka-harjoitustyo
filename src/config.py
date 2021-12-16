418126e08720e9899acbc93b3ef0a3a9fad1e567
Veera0742 <veera.lumioksa@gmail.com>
2021-12-16 10:12:50 +0200

    Changes to config

--- src/config.py
+++ src/config.py
@@ -1,12 +1,12 @@
-#import os
-#from dotenv import load_dotenv
+import os
+from dotenv import load_dotenv
 
-#dirname = os.path.dirname(__file__)
+dirname = os.path.dirname(__file__)
 
-#try:
-#    load_dotenv(dotenv_path=os.path.join(dirname, '..', '.env'))
-#except FileNotFoundError:
-#    pass
+try:
+    load_dotenv(dotenv_path=os.path.join(dirname, '..', '.env'))
+except FileNotFoundError:
+    pass
 
-#DATABASE_FILENAME = os.getenv('DATABASE_FILENAME') or 'database.sqlite'
-#DATABASE_FILE_PATH = os.path.join(dirname, '..', 'data', DATABASE_FILENAME)
+DATABASE_FILENAME = os.getenv('DATABASE_FILENAME') or 'database.sqlite'
+DATABASE_FILE_PATH = os.path.join(dirname, '..', 'data', DATABASE_FILENAME)
